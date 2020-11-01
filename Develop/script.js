b49c29efc502b8778e754620130f4698d62c26b0
TJ Hutchinson <tjhutch92@gmail.com>
2020-10-31 17:07:21 -0700

    Second Commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
-    // listen for save button clicks
+    // On clivk for save button clicks to run the function
     $(".saveBtn").on("click", function(event) {
       
       event.preventDefault();
         
-      // get nearby values
+      // grab the nearby values
       var value = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
   
-      // save in localStorage
+      // save the values in the localStorage
       localStorage.setItem(time, value);
     });
   
     function hourUpdater() {
       // set variable to current time
-      var currentHour = moment().hours();
-      //console.log(currentHour);
+      var currentHour = moment().format("hh");
+      console.log(currentHour);
 
-      // This array is the time blocks in military time
-      var blockTimes = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-      //console.log(blockTimes); 
 
       // loop over time blocks using a for loop
       $(".time-block").each(function() {
-
-        for (i=0; i < blockTimes.length; i++){
-
+    
+        // This array is the time blocks in military time
+        var blockTimes = parseInt($(this).attr("id").split("-")[1]);
+        console.log(blockTimes); 
+      
+          
             //check if we've moved past this time and changes block color to the appropiate color
-            if (blockTimes[i] < currentHour) {
-                $(this).addClass("past");
+            if (blockTimes < currentHour) { 
+              $(this).addClass("past");
             } 
-            else if (blockTimes[i] === currentHour) {
+            else if (blockTimes === currentHour) {
                 $(this).removeClass("past");
                 $(this).addClass("present");
             } 
@@ -39,14 +39,14 @@ $(document).ready(function() {
                 $(this).removeClass("present");
                 $(this).addClass("future");
             }
-        }
+        
       });
     }
   
     hourUpdater();
   
     // Interval checks to see if time needs to be updated
-    var updateInterval = setInterval(hourUpdater, 600000);
+    var updateInterval = setInterval(hourUpdater, 60000);
   
     // Saved data from localStorage is loaded
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
@@ -61,4 +61,5 @@ $(document).ready(function() {
   
     // display current day on page
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
   });
